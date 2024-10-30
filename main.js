065f8d11982ddd4f37a7947bc8281945d3568614
Fernando Cajango Dias <fernandocajango@gmail.com>
2024-10-30 13:46:55 -0300

    feat: tests + gitignore

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-console.log("Bem-vindo ao npx-easy-cli!");
+
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+
+const argv = yargs(hideBin(process.argv))
+    .command("hello", "Imprime uma saudação", {
+        name: {
+        description: "Seu nome",
+        alias: "n",
+        type: "string",
+    },
+})
+    .help().argv;
+
+if (argv._[0] === "hello") {
+    console.log(`Olá, ${argv.name || "mundo"}!`);
+}
